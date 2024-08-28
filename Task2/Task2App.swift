6d2aa9ca2ad96657bae34286045decc5b2611bcd
iOSD3v <nikkhah.fahimeh@gmail.com>
2024-08-28 12:11:30 +0330

    Initial Commit

---
+++ Task2/Task2App.swift
@@ -0,0 +1,17 @@
+//
+//  Task2App.swift
+//  Task2
+//
+//  Created by Fahimah on 8/28/24.
+//
+
+import SwiftUI
+
+@main
+struct Task2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
